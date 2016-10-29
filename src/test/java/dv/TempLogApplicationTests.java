22e41fdae2d71677e04b0bd4d44337937ec3e3a1
denis <denis.volchkov@gmail.com>
2016-10-29 15:51:59 +0300

    Initial Spring Boot app integration.

---
+++ src/test/java/dv/TempLogApplicationTests.java
@@ -0,0 +1,16 @@
+package dv;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TempLogApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

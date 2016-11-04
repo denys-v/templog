169ed2cf96a900702da9f7138feccb03ee32e99e
denis <denis.volchkov@gmail.com>
2016-11-04 07:46:59 +0200

    Spring Security basic setup.

---
+++ src/main/java/dv/config/WebSecurityConfig.java
@@ -0,0 +1,31 @@
+package dv.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("reader").password("reader").roles("READER").and()
+                .withUser("writer").password("writer").roles("WRITER", "READER").and()
+                .withUser("lelya").password("lel_0926").roles("WRITER", "READER");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                    .antMatchers("/templog/submit").hasRole("WRITER")
+                    .anyRequest().permitAll()//.hasRole("READER")
+                .and()
+                .httpBasic()
+                .and()
+                .csrf().disable();
+    }
+}
